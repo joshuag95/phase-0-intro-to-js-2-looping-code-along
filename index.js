bf2e165cd49ac277899cea2772ce15a79de3f1f6
Joshua Greer <jtgreer95@gmail.com>
2022-06-11 11:14:37 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = (['Guadalupe', 'Ollie', 'Aki'], 'surprise')
+
+function writeCards(names, event){
+    const messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+
+    } 
+return messages
+}
+
+const count1 = []
+function countDown(count1){
+    let i = 10
+    while (i >= 0){
+        console.log(i--)
+    }
+    return count1;
+}
+console.log(countDown)()
+let i = 4
+console.log(countDown)
